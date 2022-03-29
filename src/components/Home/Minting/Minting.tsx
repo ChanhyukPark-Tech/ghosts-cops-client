2b04b8c0db6ffa3fe94317b4affa26adacc5cd35
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-03-29 20:45:55 +0900

    chore: Docker test

--- src/components/Home/Minting/Minting.tsx
+++ src/components/Home/Minting/Minting.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as Styled from "./Minting.style";
 
 const Minting = () => {
-  return <Styled.Container id="minting">ssss</Styled.Container>;
+  return <Styled.Container id="minting">sssss</Styled.Container>;
 };
 
 export default Minting;
